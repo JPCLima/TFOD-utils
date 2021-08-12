ad1f006bffba14c17e55a9aa44dd0fc5ef352483
Joao Lima <joaoplima@ua.pt>
2021-08-12 17:03:34 +0200

    Edit generate_labelmap default parameter

--- generate_labelmap.py
+++ generate_labelmap.py
@@ -11,15 +11,14 @@ parser.add_argument("-l",
 
 parser.add_argument("-p",
                     "--path",
-                    default='Tensorflow\\workspace\\annotations\\label_map.pbtxt',
                     help="path of the file")
-
 
 args = parser.parse_args()
 
 
 def generate_label_map(labels, path):
-    labels_list = [{'name': label, 'id': index + 1} for index, label in enumerate(labels)]
+    labels_list = [{'name': label, 'id': index + 1}
+                   for index, label in enumerate(labels)]
 
     with open(path, 'w') as f:
         for label in labels_list:
@@ -28,8 +27,10 @@ def generate_label_map(labels, path):
             f.write('\tid:{}\n'.format(label['id']))
             f.write('}\n')
 
+
 def main():
     generate_label_map(args.labels, args.path)
+
 
 if __name__ == '__main__':
     main()
